af14dee0423fa9a566893c2f42affd7f33cc0b42
EbelVladimir <108184360+deproger@users.noreply.github.com>
2025-02-25 23:40:08 +0700

    added loaging

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
+import Loading from "./components/Loading";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  setTimeout(() => {
+    setLoading(false);
+  }, 1000);
+
   return (
-    <Router>
-      <Wrapper>
-        <Header />
+    <>
+      <Router>
+        {loading ? (
+          <Loading />
+        ) : (
+          <>
+            <Wrapper>
+              <Header />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Wrapper>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </Wrapper>
 
-      <Footer />
-    </Router>
+            <Footer />
+          </>
+        )}
+      </Router>
+    </>
   );
 };
 
---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+const Loding = () => {
+  return (
+    <>
+      <div className="h-screen w-screen flex justify-center items-center">
+        <img src="preloader.gif" width={70} alt="" />
+      </div>
+    </>
+  );
+};
+
+export default Loding;
